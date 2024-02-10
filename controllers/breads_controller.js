996af66640d1bdc81733ee7829cddee2ebabe072
Anthony <anthonylsa12@gmail.com>
2024-02-10 09:48:34 -0800

    added routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,6 +13,40 @@ breads.get('/', (req, res) => {
 })
 
 
+// New Route
+breads.get('/new', (req ,res) => {
+  res.send(render('New'))
+})
+
+// Detail Route
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.render('404').send('404. Bread not found.')
+  }
+})
+
+// Delete Route
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
+// Create Route
+breads.post('/', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread.push(req.body);
+  res.redirect('/breads');
+})
+
 // Show breads
 breads.get('/:arrayIndex', (req, res) => {
     res.render('Show', {
--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
+
+// Config
 require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
+
+// Middleware
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+
+// Breads Routes
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
